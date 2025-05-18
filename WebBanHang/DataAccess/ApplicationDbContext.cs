84cf3f6a747cdf03b4d523d684a8be48e91ce786
Douangxai <144699658+Douangxai@users.noreply.github.com>
2025-05-18 10:57:24 +0700

    Add project files.

---
+++ WebBanHang/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBanHang.Models;
+
+namespace WebBanHang.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
